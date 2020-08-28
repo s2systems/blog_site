d4b2adb5735eba9a6a9b191eefb652c23660e270
Saheed A Salam <60025414+s2systems@users.noreply.github.com>
2020-08-27 21:50:37 -0500

    Show all blog posts on the home page

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+# mysite/blog/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),   #This means if you go to http://127.0.0.1/8000. It will take you to this url
+]
