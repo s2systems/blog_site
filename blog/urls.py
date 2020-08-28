5e3e99600136c4be20e8d7116254a900cf6f5354
Saheed A Salam <60025414+s2systems@users.noreply.github.com>
2020-08-27 23:38:04 -0500

    add detail view for each log post

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),   #This means if you go to http://127.0.0.1/8000. It will take you to this url
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
