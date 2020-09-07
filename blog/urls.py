5ef4ae583dc6fc5cbb616ee8a00cf8e7ef680e8e
Saheed A Salam <60025414+s2systems@users.noreply.github.com>
2020-09-06 20:11:26 -0500

    Add feature to create new blog post from frontend

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),   #This means if you go to http://127.0.0.1/8000. It will take you to this url
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
+    path('post/new/', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 
 # Create your views here.
@@ -15,3 +16,16 @@ def post_detail(request, pk):   #Pulling post details using the primary key
     return render(request, 'blog/post_detail.html', stuff_for_frontend)
 
 
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():         #This will ensure valid information is entered
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        stuff_for_frontend = {'form': form}
+        return render(request, 'blog/post_edit.html', stuff_for_frontend)
